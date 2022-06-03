413f2c2ea48ef36c1922aa0ef349b0e249f31ea1
Guilherme Kliemann <91554258+guilhermekliemann@users.noreply.github.com>
2022-06-02 21:11:51 -0300

    Delete TrabalhoBimestralProgramacaoOrientadaObjetos2bim directory

--- TrabalhoBimestralProgramacaoOrientadaObjetos2bim/src/main/java/br/com/kliemann/sistemagerenciamentolivrarias/classes/GerenciarVenda.java
+++
@@ -1,9 +0,0 @@
-package br.com.kliemann.sistemagerenciamentolivrarias.classes;
-
-public class GerenciarVenda {
-    
-    private Livro livro;
-    private double vlrTotalVenda;
-    private int desconto;
-    
-}
